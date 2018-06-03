8b0885ec901beae2d700308c1810e5d6dc644fce
Steve Hannah <steve@weblite.ca>
2018-06-03 14:45:24 -0700

    Fixed xataface paths.

--- index.php
+++ index.php
@@ -7,5 +7,5 @@
  * This is an entry file for this Dataface Application.  To use your application
  * simply point your web browser to this file.
  */
-require_once '{__DATAFACE_PATH__}/public-api.php';
-df_init(__FILE__, '{__DATAFACE_URL__}')->display();
+require_once 'xataface/public-api.php';
+df_init(__FILE__, 'xataface')->display();
